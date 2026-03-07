ef8e495b4ff2b87613f2b7aa8b2978476069be0c
eyeadon <eyeadon40@gmail.com>
2026-03-07 13:09:49 -0500

    create and update routes for UserController

--- src/main/java/com/evan/store/controllers/UserController.java
+++ src/main/java/com/evan/store/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.evan.store.controllers;
 
+import com.evan.store.dtos.RegisterUserRequest;
+import com.evan.store.dtos.UpdateUserRequest;
 import com.evan.store.dtos.UserDto;
 import com.evan.store.mappers.UserMapper;
 import com.evan.store.repositories.UserRepository;
@@ -7,6 +9,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Set;
 
@@ -42,6 +45,33 @@ public class UserController {
 
     // var userDto = new UserDto(user.getId(), user.getName(), user.getEmail());
     // return new ResponseEntity<>(userDto, HttpStatus.OK);
+    return ResponseEntity.ok(userMapper.toDto(user));
+  }
+
+  @PostMapping
+  public ResponseEntity<UserDto> createUser(
+          @RequestBody RegisterUserRequest request,
+          UriComponentsBuilder uriBuilder
+  ) {
+    var user = userMapper.toEntity(request);
+    userRepository.save(user);
+
+    var userDto = userMapper.toDto(user);
+    var uri = uriBuilder.path("/users/{id}").buildAndExpand(userDto.getId()).toUri();
+
+    return ResponseEntity.created(uri).body(userDto);
+  }
+
+  @PutMapping("/{id}")
+  public ResponseEntity<UserDto> updateUser(@PathVariable Long id, @RequestBody UpdateUserRequest request) {
+    var user = userRepository.findById(id).orElse(null);
+    if (user == null) {
+      return ResponseEntity.notFound().build();
+    }
+
+    userMapper.update(request, user);
+    userRepository.save(user);
+
     return ResponseEntity.ok(userMapper.toDto(user));
   }
 
--- src/main/java/com/evan/store/mappers/UserMapper.java
+++ src/main/java/com/evan/store/mappers/UserMapper.java
@@ -1,10 +1,15 @@
 package com.evan.store.mappers;
 
+import com.evan.store.dtos.RegisterUserRequest;
+import com.evan.store.dtos.UpdateUserRequest;
 import com.evan.store.dtos.UserDto;
 import com.evan.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
   UserDto toDto(User user);
+  User toEntity(RegisterUserRequest request);
+  void update(UpdateUserRequest request, @MappingTarget User user);
 }
