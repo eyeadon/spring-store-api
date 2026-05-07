1ff3f23f0b92541a67c91f1579dd8fc9849718fd
eyeadon <eyeadon40@gmail.com>
2026-05-07 15:52:45 -0400

    login api; AuthenticationManager

--- src/main/java/com/evan/store/repositories/UserRepository.java
+++ src/main/java/com/evan/store/repositories/UserRepository.java
@@ -1,9 +1,15 @@
 package com.evan.store.repositories;
 
 import com.evan.store.entities.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
   boolean existsByEmail(String email);
+
+  Optional<User> findByEmail(String email);
 }
