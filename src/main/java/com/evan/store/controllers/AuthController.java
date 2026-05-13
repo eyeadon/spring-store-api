ddf475646fcb19a5a774136866e0a4d422cd601a
eyeadon <eyeadon40@gmail.com>
2026-05-13 15:41:30 -0400

    Externalizing JWT configuration

--- src/main/java/com/evan/store/controllers/AuthController.java
+++ src/main/java/com/evan/store/controllers/AuthController.java
@@ -1,5 +1,6 @@
 package com.evan.store.controllers;
 
+import com.evan.store.config.JwtConfig;
 import com.evan.store.dtos.JwtResponse;
 import com.evan.store.dtos.LoginRequest;
 import com.evan.store.dtos.UserDto;
@@ -24,6 +25,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
   private final AuthenticationManager authenticationManager;
   private final JwtService jwtService;
+  private final JwtConfig jwtConfig;
   private final UserRepository userRepository;
   private final UserMapper userMapper;
 
@@ -45,7 +47,7 @@ public class AuthController {
     var cookie = new Cookie("refreshToken", refreshToken);
     cookie.setHttpOnly(true);
     cookie.setPath("/auth/refresh");
-    cookie.setMaxAge(604800); // 7 days
+    cookie.setMaxAge(jwtConfig.getRefreshTokenExpiration());
     cookie.setSecure(true);
     response.addCookie(cookie);
 
--- src/main/java/com/evan/store/services/JwtService.java
+++ src/main/java/com/evan/store/services/JwtService.java
@@ -1,30 +1,26 @@
 package com.evan.store.services;
 
+import com.evan.store.config.JwtConfig;
 import com.evan.store.entities.User;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 
+@AllArgsConstructor
 @Service
 public class JwtService {
-  @Value("${spring.jwt.secret}")
-  private String secret;
+  private final JwtConfig jwtConfig;
 
   public String generateAccessToken(User user) {
-    final long tokenExpiration = 300; // 5 min
-
-    return generateToken(user, tokenExpiration);
+    return generateToken(user, jwtConfig.getAccessTokenExpiration());
   }
 
   public String generateRefreshToken(User user) {
-    final long tokenExpiration = 604800; // 7 days
-
-    return generateToken(user, tokenExpiration);
+    return generateToken(user, jwtConfig.getRefreshTokenExpiration());
   }
 
   private String generateToken(User user, long tokenExpiration) {
@@ -34,7 +30,7 @@ public class JwtService {
             .claim("email", user.getEmail())
             .issuedAt(new Date())
             .expiration(new Date(System.currentTimeMillis() + 1000 * tokenExpiration))
-            .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+            .signWith(jwtConfig.getSecretKey())
             .compact();
   }
 
@@ -51,7 +47,7 @@ public class JwtService {
 
   private Claims getClaims(String token) {
     return Jwts.parser()
-            .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+            .verifyWith(jwtConfig.getSecretKey())
             .build()
             .parseSignedClaims(token)
             .getPayload();
