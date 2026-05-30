3f62a739fdcd6c829ed5b0447453590263f9226e
eyeadon <eyeadon40@gmail.com>
2026-05-30 16:37:31 -0400

    Refactor the AuthController

---
+++ src/main/java/com/evan/store/auth/LoginResponse.java
@@ -0,0 +1,11 @@
+package com.evan.store.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class LoginResponse {
+  private Jwt accessToken;
+  private Jwt refreshToken;
+}
